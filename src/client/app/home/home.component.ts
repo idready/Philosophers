d1a5e21e6658cd358f46bc6a02b0f67776d1af3d
idready <you_sombo@hotmail.fr>
2017-08-16 21:27:04 +0200

    Ongoing component test

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -29,6 +29,36 @@ export class HomeComponent implements OnInit {
    */
   ngOnInit() {
     this.getNames();
+    this.testRecursion();
+  }
+
+  testRecursion() {
+
+    let stack: number[] = [];
+    let hanoi = (disc: number, src: string, aux: string, dst: string) => {
+
+      if(disc > 0) {
+        // console.log(`before recursion, disc : ${disc}`);
+        hanoi(disc - 1, src, dst, aux);// Put the disc to aux first
+        console.info(`===== Move Disc ${disc} from ${src} to ${dst} ======`);
+        hanoi(disc - 1, aux, src, dst);
+        // console.info(`Stopped disc value: ${disc}`);
+      }
+      // console.log(`after recursion, disc : ${disc}`);
+      stack.push(disc);
+    };
+
+    let testRecur = (foo: number = 4) => {
+      if(foo > 0) {
+        console.info(`${foo}`);
+        testRecur(foo - 1);
+        console.info(`After call : ${foo}`);
+      }
+    };
+
+    hanoi(3, 'source', 'aux', 'dest');
+    // testRecur(3);
+    console.info(stack);
   }
 
   /**
